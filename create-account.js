efcd8b71b9e5bbd353683e7b7237bb77bac18681
mleblanc94 <leblancmichael94@gmail.com>
2021-11-17 23:14:17 -0500

    front end login complete

--- create-account.js
+++ create-account.js
@@ -1,7 +1,15 @@
 // Establish Variables
 let resetButton = document.getElementById('reset');
-let firstName = document.getElementById('firstName-input');
-let lastName = document.getElementById('lastName-input');
+let firstNameInput = document.getElementById('firstName-input');
+let lastNameInput = document.getElementById('lastName-input');
 let usernameInput = document.getElementById('username-input');
 let passwordInput = document.getElementById('password-input');
 let submit = document.getElementById('submit');
+
+// Send the users information to the database to save the new account information
+function createAccount() {
+    let firstName = firstNameInput.value;
+    let lastName = lastNameInput.value;
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+}
