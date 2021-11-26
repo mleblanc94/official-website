bff7e2ad6b9bf4b22b6e0209e57dedc05ee1ff7f
mleblanc94 <leblancmichael94@gmail.com>
2021-11-26 17:40:07 -0500

    adding server post requests

--- server.js
+++ server.js
@@ -2,4 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
+app.get('/', (req, res)=> {
+    res.send('This is working');
+})
+
+app.post("/signin", (req, res) => {
+    res.send('signin')
+})
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+});
+
+// signin --> POST = success/fail
+// register --> POST = user
+// profile/userId --> GET = user
