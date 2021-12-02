5dd17b9a378bdb5c43bc1def7722a91ae7578759
mleblanc94 <leblancmichael94@gmail.com>
2021-12-02 18:57:18 -0500

    connected to the backend

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');({
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'your_database_user',
+      password : 'your_database_password',
+      database : 'myapp_test'
+    }
+  });
 
 
 const app = express();
