d6cc968710963f6c74ee057239550b80de0525d0
mleblanc94 <leblancmichael94@gmail.com>
2021-12-06 19:38:32 -0500

    commiting changes that were accidentally not commited after last session

--- server.js
+++ server.js
@@ -75,16 +75,10 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-       if (user.id === id) {
-           found = true;
-           return res.json(user);
-       }
-       })
-       if (!found) {
-           res.status(400).json('not found')
-       }
+    database.select('*').from('users').where({id})
+    .then(user => {
+        res.json(user[0])
+    })
 })
 
 app.listen(3000, ()=> {
