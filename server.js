de513096d282d8f565d0b3582b833700401ab9a8
mleblanc94 <leblancmichael94@gmail.com>
2021-12-16 20:12:42 -0500

    changes to app

--- server.js
+++ server.js
@@ -70,7 +70,5 @@ app.get('/profile/:id', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`app is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
 
